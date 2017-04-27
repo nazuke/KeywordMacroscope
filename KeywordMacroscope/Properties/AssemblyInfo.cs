8809a2121de81de36665da6b996e4597d827ff43
Jason Holland <nazuke.hondou@googlemail.com>
2017-04-27 11:38:32 +0900

    Created WPF project

---
+++ KeywordMacroscope/Properties/AssemblyInfo.cs
@@ -0,0 +1,30 @@
+#region Using directives
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+#endregion
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle ("KeywordMacroscope")]
+[assembly: AssemblyDescription ("")]
+[assembly: AssemblyConfiguration ("")]
+[assembly: AssemblyCompany ("")]
+[assembly: AssemblyProduct ("KeywordMacroscope")]
+[assembly: AssemblyCopyright ("Copyright 2017 Jason Holland")]
+[assembly: AssemblyTrademark ("")]
+[assembly: AssemblyCulture ("")]
+// This sets the default COM visibility of types in the assembly to invisible.
+// If you need to expose a type to COM, use [ComVisible(true)] on that type.
+[assembly: ComVisible (false)]
+// The assembly version has following format :
+//
+// Major.Minor.Build.Revision
+//
+// You can specify all the values or you can use the default the Revision and 
+// Build Numbers by using the '*' as shown below:
+[assembly: AssemblyVersion ("0.0.0.1")]
+[assembly: AssemblyFileVersion ("0.0.0.0")]
+[assembly: AssemblyInformationalVersion ("v0.0.0.1a")]
+[assembly: Guid ("b2cd8194-a4c0-4d3b-a55c-f98bfa06ae32")]
---
+++ KeywordMacroscope/version.nsh
@@ -0,0 +1,2 @@
+﻿
+!define VERSION "v0.0.0.1a"
